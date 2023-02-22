489dfee9dc9010c87259c968bafb1e17cba57cea
Panithgutta <pxg77420@ucmo.edu>
2023-02-21 20:18:49 -0600

    update

--- destination.py
+++ destination.py
@@ -1,4 +1,4 @@
-with open(destination_file_path, 'w') as f:
+with open(C:\Users\panit\OneDrive - University of Central Missouri\Desktop\destination.txt, 'w') as f:
     # Write the keys on the first line
     keys_line = ','.join(data.keys()) + '\n'
     f.write(keys_line)
