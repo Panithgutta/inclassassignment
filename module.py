b7828c3c9ad974936b2dfca756c1df9d68dfab83
Panithgutta <pxg77420@ucmo.edu>
2023-02-21 20:06:22 -0600

    changes

--- module.py
+++ module.py
@@ -1,24 +1,13 @@
 import configparser
-import json
-from file_utils import read_file
+from file_reader import read_file
 
-# Read configuration file
+# Read the configuration file
 config = configparser.ConfigParser()
 config.read('config.ini')
-source_file = config['FILES']['source_file']
-destination_file = config['FILES']['destination_file']
 
-# Read content of source file
-source_content = read_file(source_file)
-data = json.loads(source_content)
+# Get the source and destination file paths from the configuration file
+source_file_path = config['source_file']['path']
+destination_file_path = config['destination_file']['path']
 
-# Create destination file
-with open(destination_file, 'w') as file:
-    # Write keys on the first line
-    keys = list(data.keys())
-    keys_line = ','.join(keys)
-    file.write(keys_line + '\n')
-    # Write values on the second line
-    values = list(data.values())
-    values_line = ','.join(values)
-    file.write(values_line + '\n')
+# Read the content of the source file
+source_file_content = read_file(source_file_path)
