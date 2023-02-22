b09186d02f94cf65594ee241ede3bd144b090c17
Panithgutta <pxg77420@ucmo.edu>
2023-02-21 20:25:17 -0600

    changes

--- destination.py
+++
@@ -1,7 +0,0 @@
-with open(C:\Users\panit\OneDrive - University of Central Missouri\Desktop\destination.txt, 'w') as f:
-    # Write the keys on the first line
-    keys_line = ','.join(data.keys()) + '\n'
-    f.write(keys_line)
-    # Write the values on the second line
-    values_line = ','.join(data.values()) + '\n'
-    f.write(values_line)
--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,4 @@
-def read_file(C:\Users\panit\OneDrive - University of Central Missouri\Desktop\source.txt):
-    with open(C:\Users\panit\OneDrive - University of Central Missouri\Desktop\source.txt, 'r') as f:
+def read_file(C:\Users\panit\OneDrive - University of Central Missouri\Desktop\inclassassignment\data.txt):
+    with open(C:\Users\panit\OneDrive - University of Central Missouri\Desktop\inclassassignment\data.txt.txt, 'r') as f:
         content = f.read()
     return content
